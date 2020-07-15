10c3932a712b778290937164330a9f933bb96dbd
Sergio Reyes <sreyes001@gmail.com>
2020-07-15 20:39:45 -0300

    first commit

--- App.py
+++ App.py
@@ -32,66 +32,84 @@ def Index():
 def create_stocks():
 
     # read ticker symbols from the text area
-    StockTextarea = request.form['StockTextarea']
+    StockTextarea = request.form['StockTextarea'].upper()
 
     if StockTextarea.strip() == '':
         flash('Please, Input Stocks on Stock Area')
         return redirect(url_for('Index'))
 
     symbol = StockTextarea.split(',')
-
-    # Get Start Date
-    SD = ObtainDate(request.form['datefrom'])
-     
-    #Print if there is a error with the date SD
-    if SD == '':
-        return redirect(url_for('Index'))
     
     # Get End Date
-    ED = ObtainDate(request.form['dateto'])
+    ED = ObtainDate(request.form['lastdate'])
     
     #Print if there is a error with the date ED
     if ED == '':
         return redirect(url_for('Index'))
 
-    #Print if there is a error with the date ED
-    if ED <  SD:
-        flash('The end date can''t be less than the start date')
-        return redirect(url_for('Index'))        
-
-
-    # Get number of days<
-    dt = (ED - SD)
-    countDays = dt.days
-
-    # Get Current Date
+   # Get Current Date
     TD = datetime.date.today()
 
-    lSymbols = {}
-    # Generate Files Stocks
-    #location = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads') + '\\'
-    location = 'C:\\Sergio\\'
+    #Print if there is a error with the date ED
+    if ED.strftime("%Y%m%d") > TD.strftime("%Y%m%d"):
+        flash('The date can not higher than today')
+        return redirect(url_for('Index'))        
+
+    # Get number of days<
+    SD = (ED - datetime.timedelta(days=20))
+
+
+    tree_stocks = {}
 
     for stock in symbol:
-        lStock = stock.strip().upper()
         try:
-            f = web.DataReader(lStock, 'yahoo', SD, ED)
-            print('')
-            print('')
-            print(lStock)
-            print('')
-            f.loc[ED:ED, ['Close']].to_csv(location+lStock+'_'+TD.strftime("%d")+'-'+TD.strftime("%b")+'-'+TD.strftime("%Y")+'.csv', mode='w')
-            for i in range(1, countDays+1):
-                day = (ED - datetime.timedelta(days=i))
-                f.loc[day:day, ['Close']].to_csv(location+lStock+'_'+TD.strftime("%d")+'-'+TD.strftime("%b")+'-'+TD.strftime("%Y")+'.csv', mode='a', header=False)
+            #get prices info 
+            f = web.DataReader(stock, 'yahoo', SD, ED)        
+       
+            date_index = len(f)
 
-            lSymbols[lStock] = "File Created"    
+            if date_index > 0:
+                date_index = date_index - 1
+            
+            tree_stock = {}    
+
+            #Build List Dates
+            list_dates = []        
+        
+            for i in range(len(f)):
+                if i == 0:
+                    list_dates.append(f.index[date_index - i].strftime("%Y-%m-%d"))
+                else:
+                    list_dates.append(f.index[date_index - i].strftime("%Y-%m-%d"))
+
+            #Build List Prices
+            list_prices = []
+        
+            for i in range(len(f)):
+                if i == 0:
+                    list_prices.append("{:.2f}".format(f.loc[f.index[date_index - i],'Close']))
+                else:
+                    list_prices.append("{:.2f}".format(f.loc[f.index[date_index - i],'Close']))        
+
+            #tree_stocks[stock+'0'] = info_stock.info['industry']
+            #tree_stocks[stock+'1'] = info_stock.calendar.loc[info_stock.calendar.index[0],'Value'].strftime("%Y-%m-%d")
+            #tree_stocks[stock+'1'] = stock
+            tree_stock[stock+'0'] = 'DATA_FOUND'
+            tree_stock[stock+'1'] = list_dates
+            tree_stock[stock+'2'] = list_prices        
+
+            print("Symbol: " + stock)
+   
         except:
-            print("No information for symbol: " + lStock)
-            lSymbols[lStock] = "No information for symbol" 
-            continue
+            tree_stock = {}
+            tree_stock[stock+'0'] = 'NO_DATA_FOUND'
+            tree_stock[stock+'1'] = 'There is no information for the symbol'
+            tree_stocks[stock] = tree_stock
+            continue 
 
-    return render_template('create_stocks.html', pStocks=lSymbols,pLocation = location)
+        tree_stocks[stock] = tree_stock 
+
+    return render_template('create_stocks.html', pTreeStocks=tree_stocks)
 
 if __name__ == '__main__':
     # debug--> Use when we had changes into the server so restart the server
