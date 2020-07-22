b05a796f7d2f17d3fd0f0a0128d1517fac89a5ed
Sergio Reyes <sreyes001@gmail.com>
2020-07-22 14:40:40 -0300

    first commit

--- App.py
+++ App.py
@@ -94,7 +94,7 @@ def create_stocks():
     tree_stocks = {}
 
     #Set earnigs
-    yec = YahooEarningsCalendar()
+    #yec = YahooEarningsCalendar()
 
     m = Ticker(symbol)
 
@@ -105,7 +105,21 @@ def create_stocks():
 
             l_count_stock = len(f)
 
-            yec_desc = ''
+            try:
+                #get earnig date            
+                yec_date = (datetime.date.fromtimestamp(m.get_modules('calendarEvents')[stock]['earnings']['earningsDate'][0]))
+                yec_count = (yec_date - TD).days
+
+                if yec_count > 1:
+                    yec_desc = str(yec_count) + ' days to earnings call'
+                else:
+                    if yec_count >= 0:
+                        yec_desc = str(yec_count) + ' day to earnings call'
+                    else:
+                        yec_desc = ''
+
+            except:
+                yec_desc = ''
 
             try:
                 #get Name and Industry
